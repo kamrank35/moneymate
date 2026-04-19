e8b489c0bfc3328bc40343bd4b9d0c383d744a73
kamrank35 <119065949+kamrank35@users.noreply.github.com>
2026-04-19 22:38:47 +0530

    Fix: Simplify dbConfig

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,23 +1,8 @@
-// const mg = require('mongoose')
-
-// mg.connect(process.env.mongo_url)
-
-// const connectionresult = mg.connection;
-
-// connectionresult.on('error',() =>{ console.log('connection error: ')})
-// connectionresult.on('connected', () =>{console.log('Mongo DB connected successfully')})
-
-// module.exports = connectionresult;
-
-require('dotenv').config();
 const mg = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mg.connect(process.env.mongo_url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mg.connect(process.env.mongo_url);
         console.log('Mongo DB connected successfully');
     } catch (err) {
         console.error('Connection error:', err);
@@ -27,14 +12,4 @@ const connectDB = async () => {
 
 connectDB();
 
-const connectionresult = mg.connection;
-
-connectionresult.on('disconnected', () => {
-    console.log('Mongo DB disconnected');
-});
-
-connectionresult.on('reconnected', () => {
-    console.log('Mongo DB reconnected');
-});
-
-module.exports = connectionresult;
+module.exports = mg.connection;
