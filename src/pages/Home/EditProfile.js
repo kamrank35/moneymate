bede78ad752fe1e249f30f0b0d5b0f26c2c7c71e
kamrank35 <119065949+kamrank35@users.noreply.github.com>
2026-04-21 13:08:46 +0530

    Feature: Fancy edit profile modal and overflow fixes

--- src/pages/Home/EditProfile.js
+++ src/pages/Home/EditProfile.js
@@ -3,6 +3,7 @@ import { Modal, Form, message } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { UpdateProfile, UpdatePassword } from '../../apicalls/Users';
 import { SetUser } from '../../redux/usersSlice';
+import { motion } from 'framer-motion';
 
 function EditProfile({ showEditProfile, setShowEditProfile }) {
     const { user } = useSelector(state => state.users);
@@ -75,142 +76,171 @@ function EditProfile({ showEditProfile, setShowEditProfile }) {
 
     return (
         <Modal
-            title="Edit Profile"
+            title={
+                <div className="modal-title">
+                    <i className="ri-user-settings-line"></i>
+                    <span>Edit Profile</span>
+                </div>
+            }
             open={showEditProfile}
             onCancel={() => setShowEditProfile(false)}
             footer={null}
-            width={520}
+            width={500}
             className="edit-profile-modal"
         >
-            <div className="profile-tabs">
-                <button
-                    className={`tab-btn ${activeTab === 'profile' ? 'active' : ''}`}
-                    onClick={() => setActiveTab('profile')}
-                >
-                    <i className="ri-user-line"></i>
-                    Profile Info
-                </button>
-                <button
-                    className={`tab-btn ${activeTab === 'password' ? 'active' : ''}`}
-                    onClick={() => setActiveTab('password')}
-                >
-                    <i className="ri-lock-line"></i>
-                    Change Password
-                </button>
+            <div className="edit-profile-modal-body">
+                <div className="profile-tabs">
+                    <button
+                        className={`tab-btn ${activeTab === 'profile' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('profile')}
+                    >
+                        <i className="ri-user-line"></i>
+                        Profile Info
+                    </button>
+                    <button
+                        className={`tab-btn ${activeTab === 'password' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('password')}
+                    >
+                        <i className="ri-lock-line"></i>
+                        Change Password
+                    </button>
+                </div>
+
+                {activeTab === 'profile' && (
+                    <Form
+                        form={form}
+                        layout="vertical"
+                        onFinish={handleProfileUpdate}
+                        className="profile-form"
+                    >
+                        <div className="form-row">
+                            <Form.Item label="First Name" name="firstName" rules={[{ required: true }]}>
+                                <input type="text" placeholder="First name" className="modal-input" />
+                            </Form.Item>
+                            <Form.Item label="Last Name" name="lastName" rules={[{ required: true }]}>
+                                <input type="text" placeholder="Last name" className="modal-input" />
+                            </Form.Item>
+                        </div>
+
+                        <Form.Item label="Email" name="email">
+                            <input type="email" disabled placeholder="Email cannot be changed" className="modal-input disabled-input" />
+                        </Form.Item>
+
+                        <Form.Item label="Phone Number" name="phoneNumber" rules={[{ required: true }]}>
+                            <input type="tel" placeholder="Phone number" className="modal-input" />
+                        </Form.Item>
+
+                        <div className="form-row">
+                            <Form.Item label="ID Type" name="identificationType" rules={[{ required: true }]}>
+                                <select className="modal-input">
+                                    <option value="NATIONAL ID">National ID</option>
+                                    <option value="PASSPORT">Passport</option>
+                                    <option value="DRIVING LICENSE">Driving License</option>
+                                </select>
+                            </Form.Item>
+                            <Form.Item label="ID Number" name="identificationNumber" rules={[{ required: true }]}>
+                                <input type="text" placeholder="ID number" className="modal-input" />
+                            </Form.Item>
+                        </div>
+
+                        <Form.Item label="Address" name="address" rules={[{ required: true }]}>
+                            <textarea rows={2} placeholder="Full address" className="modal-input" />
+                        </Form.Item>
+
+                        <div className="modal-actions">
+                            <motion.button
+                                type="button"
+                                className="modal-cancel-btn"
+                                onClick={() => setShowEditProfile(false)}
+                                disabled={saving}
+                            >
+                                Cancel
+                            </motion.button>
+                            <motion.button
+                                type="submit"
+                                className="modal-confirm-btn"
+                                disabled={saving}
+                            >
+                                {saving ? (
+                                    <span className="btn-spinner">
+                                        <i className="ri-loader-4-line spin"></i>
+                                        Saving...
+                                    </span>
+                                ) : (
+                                    <>
+                                        <i className="ri-save-line mr-1"></i>
+                                        Save Changes
+                                    </>
+                                )}
+                            </motion.button>
+                        </div>
+                    </Form>
+                )}
+
+                {activeTab === 'password' && (
+                    <Form
+                        form={passwordForm}
+                        layout="vertical"
+                        onFinish={handlePasswordUpdate}
+                        className="profile-form"
+                    >
+                        <Form.Item
+                            label="Current Password"
+                            name="currentPassword"
+                            rules={[{ required: true, message: 'Please enter current password' }]}
+                        >
+                            <input type="password" placeholder="Enter current password" className="modal-input" />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="New Password"
+                            name="newPassword"
+                            rules={[
+                                { required: true, message: 'Please enter new password' },
+                                { min: 6, message: 'Password must be at least 6 characters' }
+                            ]}
+                        >
+                            <input type="password" placeholder="Enter new password" className="modal-input" />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Confirm New Password"
+                            name="confirmPassword"
+                            rules={[{ required: true, message: 'Please confirm password' }]}
+                        >
+                            <input type="password" placeholder="Confirm password" className="modal-input" />
+                        </Form.Item>
+
+                        <div className="modal-actions">
+                            <motion.button
+                                type="button"
+                                className="modal-cancel-btn"
+                                onClick={() => setShowEditProfile(false)}
+                                disabled={saving}
+                            >
+                                Cancel
+                            </motion.button>
+                            <motion.button
+                                type="submit"
+                                className="modal-confirm-btn"
+                                disabled={saving}
+                            >
+                                {saving ? (
+                                    <span className="btn-spinner">
+                                        <i className="ri-loader-4-line spin"></i>
+                                        Updating...
+                                    </span>
+                                ) : (
+                                    <>
+                                        <i className="ri-lock-unlock-line mr-1"></i>
+                                        Update Password
+                                    </>
+                                )}
+                            </motion.button>
+                        </div>
+                    </Form>
+                )}
             </div>
-
-            {activeTab === 'profile' && (
-                <Form
-                    form={form}
-                    layout="vertical"
-                    onFinish={handleProfileUpdate}
-                    className="profile-form"
-                >
-                    <div className="form-row">
-                        <Form.Item label="First Name" name="firstName" rules={[{ required: true }]}>
-                            <input type="text" placeholder="First name" />
-                        </Form.Item>
-                        <Form.Item label="Last Name" name="lastName" rules={[{ required: true }]}>
-                            <input type="text" placeholder="Last name" />
-                        </Form.Item>
-                    </div>
-
-                    <Form.Item label="Email" name="email">
-                        <input type="email" disabled placeholder="Email cannot be changed" />
-                    </Form.Item>
-
-                    <Form.Item label="Phone Number" name="phoneNumber" rules={[{ required: true }]}>
-                        <input type="tel" placeholder="Phone number" />
-                    </Form.Item>
-
-                    <div className="form-row">
-                        <Form.Item label="ID Type" name="identificationType" rules={[{ required: true }]}>
-                            <select>
-                                <option value="NATIONAL ID">National ID</option>
-                                <option value="PASSPORT">Passport</option>
-                                <option value="DRIVING LICENSE">Driving License</option>
-                            </select>
-                        </Form.Item>
-                        <Form.Item label="ID Number" name="identificationNumber" rules={[{ required: true }]}>
-                            <input type="text" placeholder="ID number" />
-                        </Form.Item>
-                    </div>
-
-                    <Form.Item label="Address" name="address" rules={[{ required: true }]}>
-                        <textarea rows={2} placeholder="Full address" />
-                    </Form.Item>
-
-                    <div className="form-actions">
-                        <button
-                            type="button"
-                            className="cancel-btn"
-                            onClick={() => setShowEditProfile(false)}
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            type="submit"
-                            className="save-btn"
-                            disabled={saving}
-                        >
-                            {saving ? 'Saving...' : 'Save Changes'}
-                        </button>
-                    </div>
-                </Form>
-            )}
-
-            {activeTab === 'password' && (
-                <Form
-                    form={passwordForm}
-                    layout="vertical"
-                    onFinish={handlePasswordUpdate}
-                    className="profile-form"
-                >
-                    <Form.Item
-                        label="Current Password"
-                        name="currentPassword"
-                        rules={[{ required: true, message: 'Please enter current password' }]}
-                    >
-                        <input type="password" placeholder="Enter current password" />
-                    </Form.Item>
-
-                    <Form.Item
-                        label="New Password"
-                        name="newPassword"
-                        rules={[
-                            { required: true, message: 'Please enter new password' },
-                            { min: 6, message: 'Password must be at least 6 characters' }
-                        ]}
-                    >
-                        <input type="password" placeholder="Enter new password" />
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Confirm New Password"
-                        name="confirmPassword"
-                        rules={[{ required: true, message: 'Please confirm new password' }]}
-                    >
-                        <input type="password" placeholder="Confirm new password" />
-                    </Form.Item>
-
-                    <div className="form-actions">
-                        <button
-                            type="button"
-                            className="cancel-btn"
-                            onClick={() => setShowEditProfile(false)}
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            type="submit"
-                            className="save-btn"
-                            disabled={saving}
-                        >
-                            {saving ? 'Updating...' : 'Update Password'}
-                        </button>
-                    </div>
-                </Form>
-            )}
         </Modal>
     );
 }
